29f78094d923f8e0c7773730b1b5ffb7ec1dfddd
Pratham <chudasamapratham142@gmail.com>
2023-08-09 15:47:29 +0530

    createUser function created

--- src/components/AuthSetter.tsx
+++ src/components/AuthSetter.tsx
@@ -1,22 +1,12 @@
 "use client";
+import { createUser } from "@/utils/createUser";
 import { useEffect } from "react";
 
 export default function AuthSetter() {
 	useEffect(() => {
 		if (localStorage.getItem("userId") === null) {
-			fetch("http://localhost:3000/api/user/create", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}).then((res) => {
-                if (!res.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-
-                return res.json();
-            }).then((data) => {
-                localStorage.setItem("userId", data.id);
+			createUser().then((res) => {
+                localStorage.setItem("userId", res.id);
             });
 		}
 	}, []);
--- src/utils/createUser.ts
+++ src/utils/createUser.ts
@@ -3,3 +3,12 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export async function createUser() {
+	return await prisma.user.create({
+		select: {
+			id: true,
+		},
+		data: {},
+	});
+}
