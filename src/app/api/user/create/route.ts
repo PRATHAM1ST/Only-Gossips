8f9256bbd75ebef35100ea5c887229d5847ef51c
Pratham <chudasamapratham142@gmail.com>
2024-02-12 23:53:18 +0530

    Refactor user create route to remove unnecessary code

--- src/app/api/user/create/route.ts
+++ src/app/api/user/create/route.ts
@@ -4,8 +4,5 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function POST() {
-    const result = await prisma.user.create({
-        data: {}
-    });
-    return NextResponse.json(result);
+    return;
 }
